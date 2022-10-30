f5d589f13694b2b2c1db73a9a62744de1e18039a
NithyaBalaji03 <116972375+NithyaBalaji03@users.noreply.github.com>
2022-10-30 11:39:14 +0530

    Add files via upload
    day1

---
+++ day1/Report.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Report {
+	
+	public static void main(String[] args) {
+		Student std = new Student();
+		System.out.println("Student Name = " + std.studentName);
+		System.out.println("Roll Number = " + std.rollNo);
+		System.out.println("College Name = " + std.collegeName);
+		System.out.println("Marks Scored = " + std.markScored);
+		System.out.println("cgpa = " + std.cgpa);
+	}
+}
---
+++ day1/Student.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class Student {
+
+	String studentName = "Nithya";
+	int rollNo = 12345;
+	String collegeName = "Sathybama";
+	short markScored = 89;
+	float cgpa = 8.9f;	
+}
